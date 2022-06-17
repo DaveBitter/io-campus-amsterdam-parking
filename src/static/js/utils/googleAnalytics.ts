85254a9e2fea09243e90ecaa07451138e1d90f7e
Dave Bitter <dbitter@anwb.nl>
2022-06-17 17:34:20 +0200

    feat(react-ga): update to v4

--- src/static/js/utils/googleAnalytics.ts
+++ src/static/js/utils/googleAnalytics.ts
@@ -1,19 +1,8 @@
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 export const initGA = () => {
-  ReactGA.initialize("UA-70821261-2");
+  ReactGA.initialize("G-Z1T119T2XT");
 };
 export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
-};
-export const logEvent = (category = "", action = "") => {
-  if (category && action) {
-    ReactGA.event({ category, action });
-  }
-};
-export const logException = (description = "", fatal = false) => {
-  if (description) {
-    ReactGA.exception({ description, fatal });
-  }
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 };
